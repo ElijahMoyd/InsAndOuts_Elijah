db70ad3ca86206e59abe3fbde7bbde6499f91496
ElijahMoyd <45174357+ElijahMoyd@users.noreply.github.com>
2022-04-27 10:12:45 -0400

    SS7

--- ButtonpressLED/ButtonpressLED.ino
+++ ButtonpressLED/ButtonpressLED.ino
@@ -3,7 +3,7 @@ const int LED2 = 10;
 const int LED3= 9;
 const int LED4 = 6;
 const int LED5 = 3;
-
+const int LED6 = 5;
 const int buttonPin = 13;
 
 int buttonState = 0; 
@@ -14,12 +14,13 @@ pinMode(LED,OUTPUT);
 pinMode(LED2,OUTPUT);
 pinMode(LED3,OUTPUT);
 pinMode(LED4,OUTPUT);
-
+pinMode (LED5, OUTPUT);
+pinMode (LED6, OUTPUT);
 pinMode(buttonPin,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 buttonState = digitalRead(buttonPin);
 delay(10);//small delay for debouncing to increase accuracy
 if(buttonState == HIGH){
@@ -30,7 +31,8 @@ if(buttonState == HIGH){
   analogWrite(LED2, 150);
   analogWrite(LED3, 100);
   analogWrite(LED4, 70);
-  
+  analogWrite (LED5, 50);
+  analogWrite (LED6. 30);
 }else  {
    for (int i = 0 ; i >= 100; i += 1){
     analogWrite(LED,i); 
@@ -50,5 +52,12 @@ analogWrite(LED4,0);
 delay(200); 
  analogWrite(LED4,120); 
 
+analogWrite(LED5,0); 
+delay(160); 
+ analogWrite(LED5,120); 
+
+ analogWrite(LED6,0); 
+delay(120); 
+ analogWrite(LED6,120);
 }
 }
